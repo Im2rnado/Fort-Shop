a6913273f27f2c0063a17c466b257853241312f5
Shire <70603474+ShireDev@users.noreply.github.com>
2021-07-04 21:40:15 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const { generateShop, getShopItems } = require("./shop");
-const { apiKey, language, watermark } = require("./config.json");
-
-(async () => {
-  const items = await getShopItems(apiKey, language);
-  console.log("[Fort-Shop] Made by Im2rnado. For support, join discord.gg/carbide".rainbow);
-  await generateShop(items, watermark);
-})()
