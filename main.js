983fa5444424479b51ff2b9c07b87dc823605872
Shire <70603474+ShireDev@users.noreply.github.com>
2021-07-04 21:39:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const { apiKey, language, watermark } = require("./config.json");
 
 (async () => {
   const items = await getShopItems(apiKey, language);
-  console.log("[Fort-Shop] Made by Im2rnado and Shire. For support, join discord.gg/carbide".rainbow);
+  console.log("[Fort-Shop] Made by Im2rnado. For support, join discord.gg/carbide".rainbow);
   await generateShop(items, watermark);
 })()
